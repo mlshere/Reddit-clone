221790771acdc497ee7c319490159736b504df7d
mlshere <mlsherezade@gmail.com>
2024-08-19 17:37:09 +0200

    Add bundler analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
-export default nextConfig;
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default bundleAnalyzer({
+  // Your Next.js config
+});
+
+  
