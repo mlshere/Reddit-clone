e226cc73a37db8a79e4e7500e24400de921c2976
mlshere <mlsherezade@gmail.com>
2024-08-19 17:33:02 +0200

    Delete styling files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import "./globals.css";
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '../app/chakra/theme';
 
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './app/chakra/theme';
+import Layout from '../component/Layout/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ChakraProvider theme={theme}>
-    <Component {...pageProps} />
+  <ChakraProvider theme={theme}>  
+    <Layout >
+      <Component {...pageProps} />
+    </Layout>
   </ChakraProvider>
+  
   )
 }
 
