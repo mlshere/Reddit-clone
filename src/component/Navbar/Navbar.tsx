6e19f25e62afaf94044a2b117dcdf2a8a096a266
mlshere <mlsherezade@gmail.com>
2024-08-19 17:38:25 +0200

    Fix Image width

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,11 +1,16 @@
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
+import Image from 'next/image';
 
 const Navbar: React.FC = () => { 
     return (
         <>
-        <nav>
-            <h1>Navbar</h1>
-        </nav>
+            <Flex bg="white" height="44px" padding="6px 12px">
+               <Flex>
+                <Image src="/images/redditFace.svg" height={30} width={30} alt="reddit logo face" />
+                <Image src="/images/redditText.svg" height={46} width={46} alt="reddit text" />
+                </Flex> 
+            </Flex>
         </>
     )
 }
