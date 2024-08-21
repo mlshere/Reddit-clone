1a8d4b42c9c9cde25ee652be353275fd99e51cf3
mlshere <mlsherezade@gmail.com>
2024-08-21 08:36:55 +0200

    Add Recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
 
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from './app/chakra/theme';
+import { theme } from '../app/chakra/theme';
 import Layout from '../component/Layout/Layout';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ChakraProvider theme={theme}>  
-    <Layout >
-      <Component {...pageProps} />
-    </Layout>
-  </ChakraProvider>
+  <RecoilRoot>
+    <ChakraProvider theme={theme}>  
+      <Layout >
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  </RecoilRoot>
   
   )
 }
