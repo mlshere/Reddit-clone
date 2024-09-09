45db61758a623666386f933e6dc459fc88e6fc64
mlshere <mlsherezade@gmail.com>
2024-09-09 11:52:45 +0200

    Update Auth inputs

--- src/component/Modal/Auth/AuthInputs.tsx
+++ src/component/Modal/Auth/AuthInputs.tsx
@@ -4,6 +4,7 @@ import { Sign } from 'crypto';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import  Login  from '../Login';
+import  SignUp  from '../SignUp';
 
 type AuthInputsProps = {
     
@@ -14,7 +15,7 @@ const AuthInputs: React.FC<AuthInputsProps> = () => {
     return (
         <Flex direction="column" align="center" width="100%" mt={4}>
             {modalState.view === 'login' && <Login />}
-            {/*{modalState.view === 'signup' && <Signup />} */}
+            {modalState.view === 'signup' && <SignUp />}
             
         </Flex>
     );
