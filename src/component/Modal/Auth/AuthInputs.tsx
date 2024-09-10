d8f10f29c88169879b8e03e567e0f9bb82ae5be0
mlshere <mlsherezade@gmail.com>
2024-09-10 21:05:30 +0200

    Fix Login toggleView

--- src/component/Modal/Auth/AuthInputs.tsx
+++ src/component/Modal/Auth/AuthInputs.tsx
@@ -1,4 +1,4 @@
-import { authModalState } from '@/app/atoms/authModalAtom';
+import { authModalState, ModalView } from '@/app/atoms/authModalAtom';
 import { Flex } from '@chakra-ui/react';
 import { Sign } from 'crypto';
 import React from 'react';
@@ -14,7 +14,9 @@ const AuthInputs: React.FC<AuthInputsProps> = () => {
     const modalState = useRecoilValue(authModalState);
     return (
         <Flex direction="column" align="center" width="100%" mt={4}>
-            {modalState.view === 'login' && <Login />}
+            {modalState.view === 'login' && <Login toggleView={function (view: ModalView): void {
+                throw new Error('Function not implemented.');
+            } } />}
             {modalState.view === 'signup' && <SignUp />}
             
         </Flex>
