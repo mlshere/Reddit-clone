9a2a8c8a37988cce3a166da6461284c7045a37b7
mlshere <mlsherezade@gmail.com>
2024-09-16 16:58:21 +0200

    Change Sign in to up

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,32 +1,44 @@
-import { Flex, Image} from '@chakra-ui/react';
-import React from 'react';
-import SearchInput from './SearchInput';
-import RightContent from './RightContent/RightContent';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '@/firebase/clientApp';
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import SearchInput from "./SearchInput";
+import RightContent from "./RightContent/RightContent";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
+import Directory from "./Directory/Directory";
 
-
-const Navbar: React.FC = () => { 
-    const [ user, loading, error ] = useAuthState(auth);
-    return (
-        <>
-            <Flex bg="white" height="44px" padding="6px 12px">
-               <Flex align="center">
-                <Image src="/images/redditFace.svg" height='30px' alt="reddit logo face" />
-                <Image 
-                src="/images/redditText.svg" 
-                height='46px'  
-                alt="reddit text" 
-                display={{ base: "none", md: 'unset'}}
-                />
-                </Flex> 
-            
-            {/*<Directory />*/}
-            <SearchInput />
-            <RightContent user={user}/>
-            </Flex>
-        </>
-    )
-}
+const Navbar: React.FC = () => {
+  const [user, loading, error] = useAuthState(auth);
+  return (
+    <>
+      <Flex 
+        bg="white" 
+        height="44px" 
+        padding="6px 12px"
+        justify={{ md: "space-between" }}
+        >
+        <Flex 
+            align="center"
+            width={{ base: "40px", md: "auto"}}
+            mr={{ base: 0, md: 2}}
+        >
+          <Image
+            src="/images/redditFace.svg"
+            height="30px"
+            alt="reddit logo face"
+          />
+          <Image
+            src="/images/redditText.svg"
+            height="46px"
+            alt="reddit text"
+            display={{ base: "none", md: "unset" }}
+          />
+        </Flex>
+        {user && <Directory />}
+        <SearchInput user={user} />
+        <RightContent user={user} />
+      </Flex>
+    </>
+  );
+};
 
 export default Navbar;
