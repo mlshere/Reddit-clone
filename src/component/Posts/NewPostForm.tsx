90e2c49c57dc4718b9f83e8a27f3109901a57e04
mlshere <mlsherezade@gmail.com>
2024-09-19 16:43:50 +0200

    Create submit post

---
+++ src/component/Posts/NewPostForm.tsx
@@ -0,0 +1,80 @@
+import { Flex, Icon } from "@chakra-ui/react";
+import { title } from "process";
+import React, { useState } from "react";
+import { BiPoll } from "react-icons/bi";
+import { BsLink45Deg, BsMic } from "react-icons/bs";
+import { IoDocument, IoDocumentText, IoImageOutline } from "react-icons/io5";
+import { AiFillCloseCircle } from "react-icons/ai";
+import TabItemComponent from "./TabItem";
+import TextInputs from "../Posts/PostForm/TextInputs";
+
+const formTabs: TabItem[] = [
+  {
+    title: "Post",
+    icon: IoDocumentText,
+  },
+  {
+    title: "Image & Video",
+    icon: IoImageOutline,
+  },
+  {
+    title: "Link",
+    icon: BsLink45Deg,
+  },
+  {
+    title: "Poll",
+    icon: BiPoll,
+  },
+  {
+    title: "Talk",
+    icon: BsMic,
+  },
+];
+
+export type TabItem = {
+  title: string;
+  icon: typeof Icon.arguments;
+};
+
+const NewPostForm = () => {
+  const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+const [textInputs, setTextInputs] = useState({
+    title: "",
+    body: "",
+});
+    const [selectedFile, setSelectedFile] = useState<string>();
+  const handleCreatePost = async () => {
+
+  };
+
+  const onSelectImage = () => {};
+
+  const onTextChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { target: { name, value } } = event;
+    setTextInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <Flex direction="column" bg="white" borderRadius={4} mt={2}>
+      <Flex width="100%">
+        {formTabs.map((item) => (
+          <TabItemComponent
+            item={item}
+            selected={item.title === selectedTab}
+            setSelectedTab={setSelectedTab}
+          />
+        ))}
+      </Flex>
+      <Flex p={4}>
+        <TextInputs textInputs={textInputs} handleCreatePost={handleCreatePost} onChange={onTextChange}  loading={false}/>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default NewPostForm;
