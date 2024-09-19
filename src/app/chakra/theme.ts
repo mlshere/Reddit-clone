8616bbdcebd53869992676369cc3d785188fe0e0
mlshere <mlsherezade@gmail.com>
2024-09-19 16:43:10 +0200

    Connect theme

--- src/app/chakra/theme.ts
+++ src/app/chakra/theme.ts
@@ -7,22 +7,23 @@ import { Button } from "./button";
 
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
-  colors: {
-    brand: {
-      100: "#FF3c00",
+    colors: {
+      brand: {
+        100: "#FF3C00",
+      },
     },
     fonts: {
-        body: "Open Sans, sans-serif",
+      body: "Open Sans, sans-serif",
     },
     styles: {
-        global: () => ({
-            body: {
-                bg: "gray.200",
-            }
-        })
+      global: () => ({
+        body: {
+          bg: "gray.200",
+        },
+      }),
     },
+    components: {
+      Button,
+      // Input, // not working for some reason - come back to this
     },
-   components: {
-    Button,
-    },
-})
+  });
