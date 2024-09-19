48224f12d725f2f144d4b22379c2bf1b6ad5c846
mlshere <mlsherezade@gmail.com>
2024-09-19 14:13:39 +0200

    User snippet

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,8 +6,18 @@ const db = admin.firestore();
 
 export const createUserDocument = functions.auth
   .user()
-  .onCreate(async (user: { uid: any }) => {
-    db.collection("users")
-      .doc(user.uid)
-      .set(JSON.parse(JSON.stringify(user)));
+  .onCreate(async (user) => {
+    try {
+      const newUser = {
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+        providerData: user.providerData,
+      };
+      const userRef = db.collection("users").doc(user.uid); // Declare and initialize userRef
+      await userRef.set(newUser);
+      console.log(`User ${user.uid} created`);
+    } catch (error) {
+      console.error("createUserDocument", error);
+    }
   });
