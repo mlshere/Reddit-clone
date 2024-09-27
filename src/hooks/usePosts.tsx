b2603d192c2bfc2b1a97dad75c40bd043dc8f955
mlshere <mlsherezade@gmail.com>
2024-09-27 11:49:45 +0200

    Add error handling for postId

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,6 +1,9 @@
 import { useRecoilState } from "recoil";
-import { postState } from "../app/atoms/postAtom";
+import { Post, postState } from "../app/atoms/postAtom";
 import React from "react";
+import { firestore, storage } from "../firebase/clientApp";
+import { ref, deleteObject } from "firebase/storage";
+import { deleteDoc, doc, DocumentData, DocumentReference } from "firebase/firestore";
 
 const usePosts = () => {
     const [postStateValue, setPostStateValue] = useRecoilState(postState);
@@ -11,7 +14,35 @@ const usePosts = () => {
 
     };
 
-    const onDeletePost = async () => {};
+    const onDeletePost = async (post: Post): Promise<boolean> => {
+       try {
+        // check if post has image
+        if (post.imageURL) {
+            // delete image from storage
+            const imageRef = ref(storage, `posts/${post.id}/image`);
+            await deleteObject(imageRef);
+        }
+
+        // delete post in firestore
+        
+        if (!post.id) {
+            throw new Error("Post ID is missing");
+        }
+        const postDocRef = doc(firestore, 'posts', post.id);
+        await deleteDoc(postDocRef);
+        // update recoil state without post
+
+        setPostStateValue((prev) => ({
+            ...prev,
+            posts: prev.posts.filter((p) => p.id !== post.id),
+        }));    
+
+        return true;
+       }  catch (error: any) {
+           console.log("Delete post error", error.message);
+           return false;
+       }
+    };
 
     return {
         postStateValue,
@@ -24,4 +55,8 @@ const usePosts = () => {
 
 export default usePosts;
 
+
+function delectDoc(postDocRef: DocumentReference<DocumentData, DocumentData>) {
+    throw new Error("Function not implemented.");
+}
 
