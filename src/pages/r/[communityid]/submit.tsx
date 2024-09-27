454f9f299df11bb005e9e932f79c7fb0daa938ad
mlshere <mlsherezade@gmail.com>
2024-09-27 08:39:49 +0200

    Change NewPostForm props

--- src/pages/r/[communityid]/submit.tsx
+++ src/pages/r/[communityid]/submit.tsx
@@ -1,9 +1,22 @@
 import PageContent from "@/component/Layout/PageContent";
 import NewPostForm from "@/component/Posts/NewPostForm";
 import { Box, Text } from "@chakra-ui/react";
+import { auth } from "../../../firebase/clientApp";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { communityState } from "@/app/atoms/communitiesAtom";
+import useCommunityData from "@/hooks/useCommunityData";
+import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
 
 const SubmitPostPage: React.FC = () => {
+  const [user, loadingUser] = useAuthState(auth);
+  const router = useRouter();
+  const { community } = router.query;
+  // const visitedCommunities = useRecoilValue(communityState).visitedCommunities;
+  const communityStateValue = useRecoilValue(communityState);
+  const { loading } = useCommunityData();
+  
   return (
     <PageContent >
       <>
@@ -12,11 +25,11 @@ const SubmitPostPage: React.FC = () => {
             Create a post
           </Text>
         </Box>
-        <NewPostForm />
+       {user && <NewPostForm user={user} />}
       </>
       <>
       {/* About */}
-      </>
+      </> 
     </PageContent>
   );
 };
