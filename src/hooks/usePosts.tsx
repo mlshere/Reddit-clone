ee476eb2c2a50365553f183b9a222e961ac7a0a1
mlshere <mlsherezade@gmail.com>
2024-09-28 16:00:39 +0200

    Add open authModal

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,6 +1,6 @@
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { Post, postState, PostVote } from "../app/atoms/postAtom";
-import React from "react";
+import React, { useEffect } from "react";
 import { auth, firestore, storage } from "../firebase/clientApp";
 import { ref, deleteObject } from "firebase/storage";
 import {
@@ -9,29 +9,28 @@ import {
   doc,
   DocumentData,
   DocumentReference,
+  getDocs,
+  query,
+  where,
   writeBatch,
 } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { communityState } from "@/app/atoms/communitiesAtom";
+import { authModalState } from "@/app/atoms/authModalAtom";
 
 const usePosts = () => {
   const [user] = useAuthState(auth);
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
-
+  const currentCommunity = useRecoilValue(communityState).currentCommunity;
+  const setAuthModalState = useSetRecoilState(authModalState);
   const onVote = async (post: Post, vote: number, communityId: string) => {
     console.log("Post object:", post); // Log the post object
 
     // check for user
-    if (!user) {
-        console.error("User is not authenticated.");
-        return;
-      }
-    
-      // Ensure post.id exists
-      if (!post.id) {
-        console.error("Post ID is missing");
-        return;
-      }
-
+    if(!user?.uid) {
+      setAuthModalState({ open: true, view: "login" });
+      return;
+    }
     try {
       const { voteStatus } = post; 
       const existingVote = postStateValue.postVotes.find(
@@ -156,6 +155,40 @@ const usePosts = () => {
     }
   };
 
+
+  const getCommunityPostVotes = async (communityId: string) => {
+    const postVotesQuery = query(
+      collection(firestore, "users", `${user?.uid}/postVotes`),
+      where("communityId", "==", communityId)
+    );
+
+    const postVotesDocs = await getDocs(postVotesQuery);
+    const postVotes = postVotesDocs.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    setPostStateValue((prev) => ({
+      ...prev,
+      postVotes: postVotes as PostVote[],
+    }));
+  };
+
+  useEffect(() => {
+    if (!user || !currentCommunity) return;
+    getCommunityPostVotes(currentCommunity?.id);
+  }, [user, currentCommunity]);
+
+  useEffect(() => {
+    if(!user) {
+      // clear postVotes
+      setPostStateValue((prev) => ({
+        ...prev,
+        postVotes: [],
+      }));  
+    }
+  }, [user])
+
   return {
     postStateValue,
     setPostStateValue,
@@ -166,4 +199,6 @@ const usePosts = () => {
 };
 
 export default usePosts;
+
+
 
