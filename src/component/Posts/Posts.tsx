dd29ba53cb680c940cb01bf6d18ec8980e622a64
mlshere <mlsherezade@gmail.com>
2024-09-28 16:01:26 +0200

    Check addPosts

--- src/component/Posts/Posts.tsx
+++ src/component/Posts/Posts.tsx
@@ -6,16 +6,16 @@ import {
   getDocs,
   Timestamp,
 } from "firebase/firestore";
-import { Community } from "../../app/atoms/communitiesAtom";
+import { Community, communityState } from "../../app/atoms/communitiesAtom";
 import React, { useEffect, useState } from "react";
 import { auth, firestore } from "../../firebase/clientApp";
 import usePosts from "../../hooks/usePosts";
-import { Post } from "../../app/atoms/postAtom";
+import { Post, PostVote } from "../../app/atoms/postAtom";
 import PostItem from "./PostItem";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Stack } from "@chakra-ui/react";
-import { get } from "http";
 import PostLoader from "./PostLoader";
+;
 
 type PostsProps = {
   communityData: Community;
@@ -27,6 +27,7 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
   const [user, loadingUser, error] = useAuthState(auth);
   const [loading, setLoading] = useState(false);
 
+
   const {
     postStateValue,
     setPostStateValue,
@@ -36,7 +37,6 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
   } = usePosts();
 
   const getPosts = async () => {
-    
     try {
       setLoading(true);
 
@@ -47,7 +47,6 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
         orderBy("createdAt", "desc")
       );
       const postDocs = await getDocs(postsQuery);
-      console.log("Fetched post documents:", postDocs.docs);
 
       //Store in post state
       const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
@@ -67,6 +66,26 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
     getPosts();
   }, []);
 
+  const getCommunityPostVotes = async (communityId: string) => {
+    const postVotesQuery = query(
+      collection(firestore, "users", `${user?.uid}/postVotes`),
+      where("communityId", "==", communityId)
+    );
+
+    const postVotesDocs = await getDocs(postVotesQuery);
+    const postVotes = postVotesDocs.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    setPostStateValue((prev) => ({
+      ...prev,
+      postVotes: postVotes as PostVote[],
+    }));
+  };
+
+
+
   return (
     <>
       {loading ? (
@@ -78,12 +97,15 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
               key={post.id}
               post={post}
               userIsCreator={user?.uid === post.creatorId}
-              userVoteValue={postStateValue.postVotes.find((vote) => vote.postId === post.id)?.voteValue ?? 0}
+              userVoteValue={
+                postStateValue.postVotes.find((vote) => vote.postId === post.id)
+                  ?.voteValue ?? 0
+              }
               onVote={onVote}
               onSelectPost={onSelectPost}
               onDeletePost={onDeletePost}
             />
-          ))} 
+          ))}
         </Stack>
       )}
     </>
