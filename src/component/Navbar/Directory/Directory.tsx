d7f8fd982048ef64eaf4467771e17af970aa0aba
mlshere <mlsherezade@gmail.com>
2024-10-01 18:21:22 +0200

    Update image/name in directory

--- src/component/Navbar/Directory/Directory.tsx
+++ src/component/Navbar/Directory/Directory.tsx
@@ -1,23 +1,23 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
-    Flex,
-    Icon,
-    Menu,
-    MenuButton,
-    MenuList,
-    Text
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuList,
+  Text,
+  Image,
 } from "@chakra-ui/react";
 import React from "react";
 import { TiHome } from "react-icons/ti";
 import Communities from "./Communities";
-
-  
-
+import useDirectory from "@/hooks/useDirectory";
 
 const Directory: React.FC = () => {
+  const { directoryState, toggleMenuOpen } = useDirectory();
 
   return (
-    <Menu>
+    <Menu isOpen={directoryState.isOpen}>
       <MenuButton
         cursor="pointer"
         padding="0px 6px"
@@ -25,19 +25,31 @@ const Directory: React.FC = () => {
         mr={2}
         ml={{ base: 0, md: 2 }}
         _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+        onClick={toggleMenuOpen}
       >
-        <Flex align="center" justify="space-between" width={{ base: 'auto', lg: '200 px'}}>
+        <Flex
+          align="center"
+          justify="space-between"
+          width={{ base: "auto", lg: "200 px" }}
+        >
           <Flex align="center">
-            <Icon 
-                as={TiHome} 
-                fontSize={24} 
-                mr={{ base: 1, md: 2}}
-                />
-            <Flex display={{ base: 'none', lg: "flex"}}>
-                <Text >Home</Text>
+            {directoryState.selectedMenuItem.imageURL ? (
+              <Image src={directoryState.selectedMenuItem.imageURL} borderRadius="full" boxSize="24px"  mr={2}/>
+            ) : (
+              <Icon
+                as={directoryState.selectedMenuItem.icon}
+                fontSize={24}
+                mr={{ base: 1, md: 2 }}
+                color={directoryState.selectedMenuItem.iconColor} 
+              />
+            )}
+            <Flex display={{ base: "none", lg: "flex" }}>
+              <Text fontWeight={600} fontSize="10pt">
+              {directoryState.selectedMenuItem.displayText}
+              </Text>
             </Flex>
           </Flex>
-          <ChevronDownIcon />
+          <ChevronDownIcon /> 
         </Flex>
       </MenuButton>
       <MenuList>
