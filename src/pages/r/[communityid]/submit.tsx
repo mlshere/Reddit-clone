e2582f9bc75cb2db3f2170c6536780f1f3948a06
mlshere <mlsherezade@gmail.com>
2024-10-01 18:10:43 +0200

    Clean code

--- src/pages/r/[communityid]/submit.tsx
+++ src/pages/r/[communityid]/submit.tsx
@@ -8,29 +8,32 @@ import { communityState } from "@/app/atoms/communitiesAtom";
 import useCommunityData from "@/hooks/useCommunityData";
 import { useRouter } from "next/router";
 import { useRecoilValue } from "recoil";
+import About from "@/component/Community/About";
 
 const SubmitPostPage: React.FC = () => {
   const [user, loadingUser] = useAuthState(auth);
   const router = useRouter();
   const { community } = router.query;
+  const { communityStateValue } = useCommunityData();
   // const visitedCommunities = useRecoilValue(communityState).visitedCommunities;
-  const communityStateValue = useRecoilValue(communityState);
-  console.log('COMMUNITY', communityStateValue)
+  console.log("COMMUNITY", communityStateValue);
   const { loading } = useCommunityData();
-  
+
   return (
-    <PageContent >
+    <PageContent>
       <>
         <Box>
           <Text p="14px 0px" borderBottom="1px solid" borderColor="white">
             Create a post
           </Text>
         </Box>
-       {user && <NewPostForm user={user} />}
+        {user && <NewPostForm user={user} />}
       </>
       <>
-      {/* About */}
-      </> 
+        {communityStateValue.currentCommunity && (
+          <About communityData={communityStateValue.currentCommunity} />
+        )}
+      </>
     </PageContent>
   );
 };
