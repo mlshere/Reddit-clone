b61be0b122a68a5216240e4d0dcd08b011ec4bf7
mlshere <mlsherezade@gmail.com>
2024-10-01 18:22:02 +0200

    Create menu layout

---
+++ src/component/Navbar/Directory/MenuListItem.tsx
@@ -0,0 +1,44 @@
+import useDirectory from "@/hooks/useDirectory";
+import { Flex, Icon, Image, MenuItem } from "@chakra-ui/react";
+import React from "react";
+import { IconType } from "react-icons";
+
+type MenuListItemProps = {
+  displayText: string;
+  link: string;
+  icon: IconType;
+  iconColor: string;
+  imageURL?: string;
+};
+
+const MenuListItem: React.FC<MenuListItemProps> = ({
+  displayText,
+  link,
+  icon,
+  iconColor,
+  imageURL,
+}) => {
+    const { onSelectMenuItem } = useDirectory();
+    const handleItemClick = () => {
+      onSelectMenuItem({ displayText, link, icon, iconColor, imageURL });
+    };
+  return (
+    <MenuItem
+      width="100%"
+      fontSize="10pt"
+      _hover={{ bg: "gray.100" }}
+      onClick={handleItemClick}
+    >
+      <Flex align="center">
+        {imageURL ? (
+          <Image src={imageURL} borderRadius="full" boxSize="18px" mr={2} />
+        ) : (
+          <Icon as={icon} fontSize={20} color={iconColor} mr={2} />
+        )}
+        {displayText}
+      </Flex>
+    </MenuItem>
+  );
+};
+
+export default MenuListItem;
