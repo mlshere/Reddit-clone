59b2c520dbd86bef32b45c37c23d694f718e2ffd
mlshere <mlsherezade@gmail.com>
2024-10-02 16:10:05 +0200

    Build user and non-user feed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,168 @@
+import { communityState } from "@/app/atoms/communitiesAtom";
+import { Post, PostVote } from "@/app/atoms/postAtom";
+import CreatePostLink from "@/component/Community/CreatePostLink";
+import PageContent from "@/component/Layout/PageContent";
+import PostItem from "@/component/Posts/PostItem";
+import PostLoader from "@/component/Posts/PostLoader";
+import { auth, firestore } from "@/firebase/clientApp";
+import useCommunityData from "@/hooks/useCommunityData";
+import usePosts from "@/hooks/usePosts";
+import { Stack } from "@chakra-ui/react";
+import {
+  query,
+  collection,
+  orderBy,
+  limit,
+  getDocs,
+  where,
+} from "firebase/firestore";
 import type { NextPage } from "next";
-import Image from "next/image";
+import { useEffect, useState } from "react";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilValue } from "recoil";
 
-const  Home: NextPage = () => {
+const Home: NextPage = () => {
+  const [user, loadingUser] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+  // const communityStateValue = useRecoilValue(communityState);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onSelectPost,
+    onDeletePost,
+    onVote,
+  } = usePosts();
+  const { communityStateValue } = useCommunityData();
+
+  const buildUserHomeFeed = async () => {
+    setLoading(true);
+    try {
+      if (communityStateValue.mySnippets.length) {
+        // get posts from user's communities
+        const myCommunityIds = communityStateValue.mySnippets.map(
+          (snippet) => snippet.communityId
+        );
+        const postQuery = query(
+          collection(firestore, "posts"),
+          where("communityId", "in", myCommunityIds),
+          limit(10)
+        );
+        const postDocs = await getDocs(postQuery);
+        const posts = postDocs.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPostStateValue((prev) => ({
+          ...prev,
+          posts: posts as Post[],
+        }));
+      } else {
+        buildNoUserHomeFeed();
+      }
+    } catch (error) {
+      console.log("buildUserHomeFeed error", error);
+    }
+    setLoading(false);
+  };
+
+  const buildNoUserHomeFeed = async () => {
+    setLoading(true);
+    try {
+      const postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        limit(10)
+      );
+
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: posts as Post[],
+      }));
+      // setPostState
+    } catch (error) {
+      console.log("buildNoUserHomeFeed error", error);
+    }
+    setLoading(false);
+  };
+
+  const getUserPostVotes = async () => {
+    try {
+      const postIds = postStateValue.posts.map((post) => post.id);
+      const postVotesQuery = query(
+        collection(firestore, `users/${user?.uid}/postVotes`),
+        where("postId", "in", postIds)
+      );
+      const postVotesDocs = await getDocs(postVotesQuery);
+      const postVotes = postVotesDocs.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setPostStateValue((prev) => ({
+        ...prev,
+        postVotes: postVotes as PostVote[],
+      }));
+    } catch (error) {
+      console.log("getUserPostVotes error", error);
+    }
+  };
+
+  //useEffect fot data flow
+  useEffect(() => {
+    if (communityStateValue.snippetsFetched) {
+      buildUserHomeFeed();
+    }
+  }, [communityStateValue.mySnippets]);
+
+  useEffect(() => {
+    if (!user && !loadingUser) {
+      buildNoUserHomeFeed();
+      return;
+    }
+  }, [user, loadingUser]);
+
+  useEffect(() => {
+    if (user && postStateValue.posts.length) getUserPostVotes();
+    return () => {
+      setPostStateValue(prev => ({
+        ...prev,
+        postVotes: [],
+      }));
+    };
+  }, [user, postStateValue.posts]);
+
   return (
-    <div>
-      Home
-    </div>
+    <PageContent>
+      <>
+        <CreatePostLink />
+        {loading ? (
+          <PostLoader />
+        ) : (
+          <Stack>
+            {postStateValue.posts.map((post) => (
+              <PostItem
+                key={post.id}
+                post={post}
+                onSelectPost={onSelectPost}
+                onDeletePost={onDeletePost}
+                onVote={onVote}
+                userVoteValue={
+                  postStateValue.postVotes.find(
+                    (item) => item.postId === post.id
+                  )?.voteValue ?? 0
+                }
+                userIsCreator={user?.uid === user?.uid}
+                homePage
+              />
+            ))}
+          </Stack>
+        )}
+      </>
+      <>{/* Recommendations */}</>
+    </PageContent>
   );
-}
+};
 
 export default Home;
